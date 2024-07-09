d1b3eb0d640a0cefdc2b5cacbd8b46ccdc8576fe
Xinrui Yi <1327803942@qq.com>
2024-07-09 14:07:44 +0800

    差一个模块化设置

---
+++ src/main/java/cn/helloworld1999/view/GUI.java
@@ -0,0 +1,15 @@
+package cn.helloworld1999.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+    @Override
+    public void start(Stage stage) throws Exception{
+        stage.setTitle("no");
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
